4f18c50c769e9f67a04666f3b28579518d7f431c
Jacob Langley <jacob.langley@ghtns.com>
2022-02-13 15:36:52 -0800

    Add python script for sites to kml

---
+++ bin/sites2kml.py
@@ -0,0 +1,19 @@
+#!python3
+
+import frontmatter
+import os
+from pathlib import Path
+import simplekml
+
+sites = simplekml.Kml()
+
+p = Path('sites')
+for d in p.iterdir():
+	with open(d) as f:
+		site = frontmatter.load(f)
+
+		coordinates = site['Coordinates'].split(',')
+		sites.newpoint(name=site['Name'], coords=[coordinates])
+
+
+sites.save("sites.xml")

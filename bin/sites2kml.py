d29c937ca54ba1a4c411e432e9a8ecec31a40241
Jacob Langley <jacob.langley@ghtns.com>
2022-02-13 17:57:42 -0800

    Add a name to the KML

--- bin/sites2kml.py
+++ bin/sites2kml.py
@@ -1,11 +1,12 @@
 #!python3
 
+from datetime import date
 import frontmatter
 import os
 from pathlib import Path
 import simplekml
 
-sites = simplekml.Kml()
+sites = simplekml.Kml(name="Washington Birding Site Impressions, " + date.today().strftime("%Y/%m/%d"))
 
 p = Path('_sites')
 for d in p.iterdir():
